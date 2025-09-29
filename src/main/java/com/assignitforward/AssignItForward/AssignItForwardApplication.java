95b72f1958387965ecea553c13fc06c8ccd0d8b3
Sudarshan Potdar <sudarshan121212@gmail.com>
2025-09-29 08:27:10 -0500

    Setting up Spring project

---
+++ src/main/java/com/assignitforward/AssignItForward/AssignItForwardApplication.java
@@ -0,0 +1,13 @@
+package com.assignitforward.AssignItForward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignItForwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignItForwardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignitforward/AssignItForward/AssignItForwardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignitforward.AssignItForward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignItForwardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
